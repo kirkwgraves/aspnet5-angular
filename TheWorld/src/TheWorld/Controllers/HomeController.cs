a242fca262dc3a48650ad1ead367acc8fde31629
Kirk Walker Graves <graves.kirk@gmail.com>
2016-05-10 12:26:53 -0700

    Implemented Contact Page with model validation and created MailService

--- TheWorld/src/TheWorld/Controllers/HomeController.cs
+++ TheWorld/src/TheWorld/Controllers/HomeController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using TheWorld.ViewModels;
+using TheWorld.Services;
 
 namespace TheWorld.Controllers
 {
     public class HomeController : Controller
     {
+        private IMailService _mailService;
+
+        public HomeController(IMailService service)
+        {
+            _mailService = service;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -22,7 +31,12 @@ namespace TheWorld.Controllers
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
 
             return View();
         }
